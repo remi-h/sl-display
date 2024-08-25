baff999d0c9b3188493d025870052bc3904ad812
Remi Higuchi <remyenar@yahoo.com>
2024-08-25 14:12:08 +0200

    versioning

---
+++ app/Components/VersionLabel.tsx
@@ -0,0 +1,7 @@
+export function VersionLabel() {
+    return (
+        <div className="inline bg-white text-black p-1 absolute bottom-0 right-0">
+            Ver. 0.0.1
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 import React, { useEffect, useState } from "react";
+import { VersionLabel } from "./Components/VersionLabel";
 import { Departures } from "./Components/Departures";
 
 const Home: React.FC = () => {
   return (
-    <main className="p-24 text-md">
-      Train from Bergshamra
-      <Departures />
-    </main>
+    <>
+      <main className="p-24 text-md">
+        Train from Bergshamra
+        <Departures />
+      </main>
+      <VersionLabel />
+    </>
   );
 }
 
